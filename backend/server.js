0b4462538cb439370891ad40d0b764940ab2f886
varshiniui <varshini562021@gmail.com>
2026-07-14 16:13:46 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://ledger-zeta-gules.vercel.app/',
+    'https://ledger-zeta-gules.vercel.app',
   ],
 }));
 app.use(express.json());
